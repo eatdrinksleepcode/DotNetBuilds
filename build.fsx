6bc669a07640d369d75b14a666c3cc4b8ec0db49
David Nelson <eatdrinksleepcode@gmail.com>
2016-10-26 07:59:47 -0500

    WIP

--- build.fsx
+++ build.fsx
@@ -2,18 +2,20 @@
 open Fake
 open Fake.AssemblyInfoFile
 
+let sln = ["DotNetBuilds.sln"]
+
 Target "SetAssemblyVersion" (fun _ ->
   CreateCSharpAssemblyInfo "ConsoleApp\Properties\AssemblyInfo.cs"
     [Attribute.Version "1.1"]
 )
 
 Target "Clean" (fun _ ->
-  MSBuildRelease "" "Clean" ["DotNetBuilds.sln"]
+  MSBuildRelease "" "Clean" sln
     |> Log "AppBuild-Output: "
 )
 
 Target "Build" (fun _ ->
-  MSBuildRelease "" "Build" ["DotNetBuilds.sln"]
+  MSBuildRelease "" "Build" sln
     |> Log "AppBuild-Output: "
 )
 
